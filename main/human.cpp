db8af9b6f6b0cd2a6776de1c75b775bfd0cc767c
ThesoulOfPixels <sofpixaol@gmail.com>
2021-04-15 23:09:06 +0800

    A few fixes

--- main/human.cpp
+++ main/human.cpp
@@ -1,5 +1,6 @@
-//human.cpp
+//human.cpp started at 03/28/21
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <cstdlib>//For rand
@@ -43,223 +44,16 @@ human zzx
 vector<human> humans;
 
 //Functions
-
-//Output a human's information
-void info(human man)
-{
-  const string m = "man", w = "woman";
-  string rs;
-  if (man.sex == 0)
-    {
-      rs = m;
-    } else {
-    rs = w;
-  }
-  cout << "Name: " << man.name << endl
-       << "Sex: " << rs << endl
-       << "Old: " << man.old << endl
-       << "Money: " << man.money << endl
-       << "Alive: " << man.alive << endl
-       << "Level: " << man.level << endl;
-}
-
-//Output all information
-int info_all(bool s)
-{
-  int livings = 0;
-  for (auto pr : humans)
-    if (pr.alive)
-      livings += 1;
-  if (s == true)
-    {
-      cout << "WorldTime: " << world_time << endl;
-      cout << "Now there are " << humans.size() << " humans, " << livings << " living humans." << endl
-           << "Living humans are: ";
-      for (decltype(humans.size()) index = 0; index != humans.size(); index++)
-        {
-          if (humans[index].alive)
-            cout << index << "."<<  humans[index].name << " ";
-        }
-      cout << endl;
-    }
-  return livings;
-}
-
-//Random born
-void rand_born()
-{
-  //rand
-  unsigned seed;
-  seed = time(0);
-  srand(seed);
-
-  human man;
-  cin.get();
-  man.name = "unfinded";
-  int temp = rand() % 2;
-  man.sex = humansex(temp);
-  man.old = 0;
-  man.money = rand() % 95001 + 5000;
-  man.alive = true;
-  man.level = rand() % 6 + 1;
-
-  info(man);
-
-  cout << "Enter it's name: ";
-  getline(cin, man.name);
-
-  info(man);
-  humans.push_back(man);
-}
-
-//Born
-void born()
-{
-  human man;
-
-  cin.get();
-  cout << "\nEnter it's name(string): ";
-  getline(cin, man.name);
-
-  int temp_sex;
-  cout << "\nEnter it's sex(0-man,1-woman): ";
-  cin >> temp_sex;
-  man.sex = humansex(temp_sex);
-
-  cout << "\nEnter it's old(int): ";
-  cin >> man.old;
-
-  cout << "\nEnter it's money(unsiged): ";
-  cin >> man.money;
-
-  man.alive = true;
-
-  cin.clear();
-  cout << "\nEnter it's level(1-10): ";
-  unsigned temp_level;
-  while (cin >> temp_level)
-    {
-      if (temp_level <= 10 && temp_level >= 1)
-        man.level = temp_level;
-      else
-        cout << "Please enter a number from 1 to 10!\n";
-    }
-
-  info(man);
-  humans.push_back(man);
-}
-
-//Kill
-void kill(decltype(humans.size()) index)
-{
-  auto iter = humans.begin() + index;
-  humans.erase(iter);
-  info_all(true);
-}
-
-//Power
-double powerof(human man)
-{
-  double power = man.level + man.money / 10000 + man.old / 5;
-  return power;
-}
-
-//Compare
-bool compare(human a, human b)
-{
-  double pa = powerof(a);
-  double pb = powerof(b);
-  bool result = pa > pb;
-  return result;
-}
-
-//Random battle
-void rand_fight(decltype(humans.size()) ina)
-{
-  //rand
-  unsigned seed;
-  seed = time(0);
-  srand(seed);
-
-  human &a = humans[ina];
-  decltype(humans.size()) inb = rand() % humans.size();
-  while(ina == inb | humans[inb].alive == false)//Except itself and the dead
-    {
-      inb = rand() % humans.size();
-    }
-  human &b = humans[inb];
-
-  cout << a.name << " had a fight with " << b.name << " ...\n";
-  cout << a.name;
-  if (compare(a,b))
-    {
-      cout << " winned!\n";
-      if (a.level < 10)
-        a.level++;
-      a.money += b.money * 7 / 10;
-      b.money = b.money * 3 / 10;
-      b.alive = false;
-    } else {
-      cout << " lost!\n";
-      b.money += a.money * 7 / 10;
-      a.money = a.money * 3 / 10;
-      a.alive = false;
-    }
-}
-
-//Fight
-void fight(decltype(humans.size()) ina, decltype(humans.size()) inb)
-{
-  human &a = humans[ina], &b = humans[inb];
-
-  cout << a.name << " had a fight with " << b.name << " ...\n";
-  cout << a.name;
-  if (compare(a,b))
-    {
-      cout << " winned!\n";
-      if (a.level < 10)
-        a.level++;
-      a.money += b.money * 7 / 10;
-      b.money = b.money * 3 / 10;
-      b.alive = false;
-    } else {
-    cout << " lost!\n";
-    b.money += a.money * 7 / 10;
-    a.money = a.money * 3 / 10;
-    a.alive = false;
-  }
-}
-//New year
-void year()
-{
-  for (auto &p : humans)
-    {
-      if (p.alive)
-        {
-          p.old++;
-
-          //Money
-          double dolt = rand() % 100 + 1;
-          int delta_money = rand() % (10000 * p.level / 2) + 5000 * p.level / 2;
-          p.money += delta_money;
-          p.money += dolt / 100;
-          delta_money = rand() % 28000 * p.level / 3 + 3000 * p.level / 2;
-          p.money -= delta_money;
-
-          //Luck
-          int chance = rand() % 9 + 1;
-          if (chance == 2)
-            {
-              cout << "\t\aThis year " << p.name << " had a good luck!" << endl;
-              p.money += (20000 * p.level / 2);
-              if (p.level < 10)
-                p.level ++;
-            }
-
-        }
-    }
-}
-
+void info(human man);
+int info_all(bool s);
+void rand_born();
+void born();
+void kill(decltype(humans.size()) index);
+double powerof(const human &man);
+bool compare(const human &a, const human &b);
+void rand_fight(decltype(humans.size()) ina);
+void fight(decltype(humans.size()) ina, decltype(humans.size()) inb);
+void year();
 
 int main()
 {
@@ -355,7 +149,7 @@ int main()
             cin.clear();
 
             cin >> keyword;
-            //cout << keyword << endl; -Debug
+            //cout << keyword << endl; //Debug
             if (keyword == "info")
               {
                 cin >> index;
@@ -437,4 +231,210 @@ int main()
   while (choice != 'e' && keyword != "exit");
   cout << '\a';
   return 0;
+}
+
+//Output a human's information
+void info(human man)
+{
+  const string m = "man", w = "woman";
+  string rs;
+  if (man.sex == 0)
+    {
+      rs = m;
+    } else {
+    rs = w;
+  }
+  cout << "Name: " << man.name << endl
+       << "Sex: " << rs << endl
+       << "Old: " << man.old << endl
+       << "Money: " << man.money << endl
+       << "Alive: " << man.alive << endl
+       << "Level: " << man.level << endl;
+}
+//Output all information
+int info_all(bool s)
+{
+  int livings = 0;
+  for (auto pr : humans)
+    if (pr.alive)
+      livings += 1;
+  if (s == true)
+    {
+      cout << "WorldTime: " << world_time << endl;
+      cout << "Now there are " << humans.size() << " humans, " << livings << " living humans." << endl
+           << "Living humans are: ";
+      for (decltype(humans.size()) index = 0; index != humans.size(); index++)
+        {
+          if (humans[index].alive)
+            cout << index << "."<<  humans[index].name << " ";
+        }
+      cout << endl;
+    }
+  return livings;
+}
+//Born
+void born()
+{
+  human man;
+
+  cin.get();
+  cout << "\nEnter it's name(string): ";
+  getline(cin, man.name);
+
+  int temp_sex;
+  cout << "\nEnter it's sex(0-man,1-woman): ";
+  cin >> temp_sex;
+  man.sex = humansex(temp_sex);
+
+  cout << "\nEnter it's old(int): ";
+  cin >> man.old;
+
+  cout << "\nEnter it's money(unsiged): ";
+  cin >> man.money;
+
+  man.alive = true;
+
+  cin.clear();
+  cout << "\nEnter it's level(1-10): ";
+  unsigned temp_level;
+  while (cin >> temp_level)
+    {
+      if (temp_level <= 10 && temp_level >= 1)
+        man.level = temp_level;
+      else
+        cout << "Please enter a number from 1 to 10!\n";
+    }
+
+  info(man);
+  humans.push_back(man);
+}
+//Random born
+void rand_born()
+{
+  //rand
+  unsigned seed;
+  seed = time(0);
+  srand(seed);
+
+  human man;
+  cin.get();
+  man.name = "unfinded";
+  int temp = rand() % 2;
+  man.sex = humansex(temp);
+  man.old = 0;
+  man.money = rand() % 95001 + 5000;
+  man.alive = true;
+  man.level = rand() % 6 + 1;
+
+  info(man);
+
+  cout << "Enter it's name: ";
+  getline(cin, man.name);
+
+  info(man);
+  humans.push_back(man);
+}
+//Kill
+void kill(decltype(humans.size()) index)
+{
+  auto iter = humans.begin() + index;
+  humans.erase(iter);
+  info_all(true);
+}
+//Power
+double powerof(const human &man)
+{
+  double power = man.level + man.money / 10000 + man.old / 5;
+  return power;
+}
+//Compare
+bool compare(const human &a, const human &b)
+{
+  double pa = powerof(a);
+  double pb = powerof(b);
+  bool result = pa > pb;
+  return result;
+}
+//Random fight
+void rand_fight(decltype(humans.size()) ina)
+{
+  //rand
+  unsigned seed;
+  seed = time(0);
+  srand(seed);
+
+  human &a = humans[ina];
+  decltype(humans.size()) inb = rand() % humans.size();
+  while(ina == inb | humans[inb].alive == false)//Except itself and the dead
+    inb = rand() % humans.size();
+  human &b = humans[inb];
+
+  cout << a.name << " had a fight with " << b.name << " ...\n";
+  cout << a.name;
+  if (compare(a,b))
+    {
+      cout << " winned!\n";
+      if (a.level < 10)
+        a.level++;
+      a.money += b.money * 7 / 10;
+      b.money = b.money * 3 / 10;
+      b.alive = false;
+    } else {
+    cout << " lost!\n";
+    b.money += a.money * 7 / 10;
+    a.money = a.money * 3 / 10;
+    a.alive = false;
+  }
+}
+
+//Fight
+void fight(decltype(humans.size()) ina, decltype(humans.size()) inb)
+{
+  human &a = humans[ina], &b = humans[inb];
+
+  cout << a.name << " had a fight with " << b.name << " ...\n";
+  cout << a.name;
+  if (compare(a,b))
+    {
+      cout << " winned!\n";
+      if (a.level < 10)
+        a.level++;
+      a.money += b.money * 7 / 10;
+      b.money = b.money * 3 / 10;
+      b.alive = false;
+    } else {
+    cout << " lost!\n";
+    b.money += a.money * 7 / 10;
+    a.money = a.money * 3 / 10;
+    a.alive = false;
+  }
+}
+//New year
+void year()
+{
+  for (auto &p : humans)
+    {
+      if (p.alive)
+        {
+          p.old++;
+
+          //Money
+          double dolt = rand() % 100 + 1;
+          int delta_money = rand() % (10000 * p.level / 2) + 5000 * p.level / 2;
+          p.money += delta_money;
+          p.money += dolt / 100;
+          delta_money = rand() % 28000 * p.level / 3 + 3000 * p.level / 2;
+          p.money -= delta_money;
+
+          //Luck
+          int chance = rand() % 9 + 1;
+          if (chance == 2)
+            {
+              cout << "\t\aThis year " << p.name << " had a good luck!" << endl;
+              p.money += (20000 * p.level / 2);
+              if (p.level < 10)
+                p.level ++;
+            }
+        }
+    }
 }
